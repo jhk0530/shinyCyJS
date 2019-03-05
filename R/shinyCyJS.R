3933f68fda006a15507cdb8592a64aa8493b7a50
jhk0530 <hwanistic@gmail.com>
2019-03-05 18:05:26 +0900

    0.0.2
    1. BuildNode, BuildEdge function added
    2. element styling applied

--- R/shinyCyJS.R
+++ R/shinyCyJS.R
@@ -40,7 +40,6 @@ buildROptions = function(...){
   options[which(!sapply(options, is.null))] # if option not given : remove
 }
 
-
 #' @export
 ShinyCyJSOutput = function(outputId, width = '100%', height = '400px'){
   htmlwidgets::shinyWidgetOutput(outputId, 'shinyCyJS', width, height, package = 'shinyCyJS')
@@ -50,6 +49,57 @@ ShinyCyJSOutput = function(outputId, width = '100%', height = '400px'){
 renderShinyCyJS = function(expr, env = parent.frame(), quoted = FALSE){
   if(!quoted){ expr = substitute(expr) } # force quoted
   htmlwidgets::shinyRenderWidget(expr, ShinyCyJSOutput, env, quoted = TRUE)
+}
+
+#' @export
+buildNode = function(...){
+  options = as.list(match.call())[-1] # remove function name
+  l$group = 'nodes'
+  no = names(options)
+  if(!'id' %in% no){ print("id must be exist, return")}
+  if(!'width' %in% no){ options$width = 30 }
+  if(!'height' %in% no){ options$height = 30 }
+  if(!'shape' %in% no){ options$shape = 'ellipse' }
+  if(!'bgColor' %in% no){ options$bgColor = '#48dbfb' } # megaman
+  if(!'bgOpacity' %in% no){ options$bgOpacity = 1 }
+  if(!'borderWidth' %in% no){ options$borderWidth = 0}
+  if(!'borderStyle' %in% no){ options$borderStyle = 'solid'}
+  if(!'borderColor' %in% no){ options$borderColor = '#8395a7'} # storm petrel
+  if(!'borderOpacity' %in% no){ options$borderOpacity = 1}
+  options$label = options$id
+  if(!'labelColor' %in% no){ options$labelColor = '#8395a7'}
+  if(!'textOpacity' %in% no){ options$textOpacity = 1}
+  if(!'fontSize' %in% no){ options$fontSize = 16}
+  if(!'textOutlineColor' %in% no){ options$textOutlineColor = '#222f3e'} # impreial primer
+  if(!'textOutlineOpacity' %in% no){ options$textOutlineOpacity = 1}
+  if(!'textOutlineWidth' %in% no){ options$textOutlineWidth = 0}
+  if(!'textbgColor' %in% no){ options$textbgColor = '#FFF'}
+  if(!'textbgOpacity' %in% no){ options$textbgOpacity = 0 }
+  if(!'textBorderColor' %in% no){ options$textBorderColor = '#222f3e'} # impreial primer
+  if(!'textBorderOpacity' %in% no){ options$textBorderOpacity = 0}
+  if(!'textBorderWidth' %in% no){ options$textBorderWidth = 0}
+
+  l$data = options
+
+  l
+}
+
+#' @export
+buildEdge = function(...){
+  options = as.list(match.call())[-1] # remove function name
+  l$group = 'edges'
+  no = names(options)
+
+  if(!'width' %in% no){ options$width = 3 }
+  if(!'curveStyle' %in% no){ options$curveStyle = 'haystack' }
+  if(!'lineColor' %in% no){ options$lineColor = '#feca57' }  # casandora yellow
+  if(!'lineStyle' %in% no){ options$lineStyle = 'solid'}
+  if(!'sourceArrowColor' %in% no){ options$sourceArrowColor = '#feca57'}
+  if(!'targetArrowColor' %in% no){ options$targetArrowColor = '#feca57'}
+  if(!'sourceArrowShape' %in% no){ options$sourceArrowShape = 'none'}
+  if(!'targetArrowShape' %in% no){ options$targetArrowShape = 'none'}
+  l$data = options
+  l
 }
 
 
--- inst/htmlwidgets/shinyCyJS.js
+++ inst/htmlwidgets/shinyCyJS.js
@@ -6,7 +6,45 @@ HTMLWidgets.widget({
   factory : function(el, width, height) {
 
 	// Initialisation
-	var cy = cytoscape({ container : document.getElementById(el.id) });
+	var cy = cytoscape({ 
+	container : document.getElementById(el.id),
+
+	style: cytoscape.stylesheet()
+		.selector('node').css({
+			'width': 'data(width)',
+			'height': 'data(height)',
+			'shape': 'data(shape)',
+			'background-color': 'data(bgColor)',				
+			'background-opacity': 'data(bgOpacity)',
+			'background-fill': 'data(bgFill)',
+			'border-width': 'data(borderWidth)',
+			'border-style': 'data(borderStyle)',
+			'border-color': 'data(borderColor)',
+			'border-opacity': 'data(borderOpacity)',
+			'label': 'data(label)',
+			'color': 'data(labelColor)',
+			'text-opacity': 'data(textOpacity)',
+			'font-size': 'data(fontSize)',				
+			'text-outline-color': 'data(textOutlineColor)',
+			'text-outline-opacity': 'data(textOutlineOpacity)',
+			'text-outline-width': 'data(textOutlineWidth)',
+			'text-background-color': 'data(textbgColor)',
+			'text-background-opacity': 'data(textbgOpacity)',
+			'text-border-opacity': 'data(textBorderOpacity)',
+			'text-border-width': 'data(textBorderWidth)',
+			'text-border-color': 'data(textBorderColor)',
+		})
+		.selector('edge').css({
+			'width': 'data(width)',
+			'curve-style': 'data(curveStyle)',
+			'line-color': 'data(lineColor)',
+			'line-style': 'data(lineStyle)',
+			'source-arrow-color': 'data(sourceArrowColor)',
+			'source-arrow-shape': 'data(sourceArrowShape)',
+			'target-arrow-color': 'data(targetArrowColor)',
+			'target-arrow-shape': 'data(targetArrowShape)',
+		})	
+	});
 	myCytoscape = cy;
 
     return {
@@ -19,8 +57,9 @@ HTMLWidgets.widget({
 		for(var key in Roptions){ myCytoscape.renderer()[[key]] = Roptions[key] }
 
 		// add elements
-		cy.add(input.elements)
-
+		var Elements = input.elements;
+		cy.add(Elements);
+		
 		// set Layout
 		var Layout = input.layout
 		myCytoscape.layout(Layout).run()
