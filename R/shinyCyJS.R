839ecf830ccf809f8fbd20aabc5054faa73144b6
jhk0530 <hwanistic@gmail.com>
2019-11-07 14:56:33 +0900

    Update shinyCyJS.R
    appliled styleR.

--- R/shinyCyJS.R
+++ R/shinyCyJS.R
@@ -1,70 +1,76 @@
 #' @import htmlwidgets
 
 #' @export
-shinyCyJS = function(elements = list(), options = list(),layout = list(name='cose'),
-                     width = NULL, height = NULL, elementId = NULL){
-
-  input = list( elements = elements, options = options, layout = layout )
+shinyCyJS <- function(elements = list(), options = list(), layout = list(name = "cose"),
+                      width = NULL, height = NULL, elementId = NULL) {
+  input <- list(elements = elements, options = options, layout = layout)
 
   htmlwidgets::createWidget(
-    name = 'shinyCyJS',
+    name = "shinyCyJS",
     input,
     width = width,
     height = height,
-    package = 'shinyCyJS',
+    package = "shinyCyJS",
     elementId = elementId
   )
-
 }
 
 #' @export
-buildIOptions = function(...){
+buildIOptions <- function(...) {
   # touchTapThreshold, desktopTapthreshold not available
-  options = as.list(match.call())[-1] # remove function name
-  options = options[
-    c('minZoom','maxZoom','zoomingEnabled',
-      'userZoomingEnabled','panningEnabled','userPanningEnabled',
-      'boxSelectionEnabled','selectionType','autolock',
-      'autoungrabify', 'autounselectify')]
+  options <- as.list(match.call())[-1] # remove function name
+  options <- options[
+    c(
+      "minZoom", "maxZoom", "zoomingEnabled",
+      "userZoomingEnabled", "panningEnabled", "userPanningEnabled",
+      "boxSelectionEnabled", "selectionType", "autolock",
+      "autoungrabify", "autounselectify"
+    )
+  ]
   options[which(!sapply(options, is.null))] # if option not given : remove
 }
 
 #' @export
-buildROptions = function(...){
-  options = as.list(match.call())[-1] # remove function name
-  options = options[
-    c('headless','styleEnabled','hideEdgesOnViewport',
-      'hideLabelsOnViewport','textureOnViewport','motionBlur',
-      'motionBlurOpacity','wheelSensitivity','pixelRatio')]
+buildROptions <- function(...) {
+  options <- as.list(match.call())[-1] # remove function name
+  options <- options[
+    c(
+      "headless", "styleEnabled", "hideEdgesOnViewport",
+      "hideLabelsOnViewport", "textureOnViewport", "motionBlur",
+      "motionBlurOpacity", "wheelSensitivity", "pixelRatio"
+    )
+  ]
   options[which(!sapply(options, is.null))] # if option not given : remove
 }
 
 #' @export
-ShinyCyJSOutput = function(outputId, width = '100%', height = '400px'){
-  htmlwidgets::shinyWidgetOutput(outputId, 'shinyCyJS', width, height, package = 'shinyCyJS')
+ShinyCyJSOutput <- function(outputId, width = "100%", height = "400px") {
+  htmlwidgets::shinyWidgetOutput(outputId, "shinyCyJS", width, height, package = "shinyCyJS")
 }
 
 #' @export
-renderShinyCyJS = function(expr, env = parent.frame(), quoted = FALSE){
-  if(!quoted){ expr = substitute(expr) } # force quoted
+renderShinyCyJS <- function(expr, env = parent.frame(), quoted = FALSE) {
+  if (!quoted) {
+    expr <- substitute(expr)
+  } # force quoted
   htmlwidgets::shinyRenderWidget(expr, ShinyCyJSOutput, env, quoted = TRUE)
 }
 
 #' @export
-buildNode = function(
-  id, width = 15, height = 15, shape = 'ellipse', bgColor = '#48DBFB',
-  bgOpacity = 1, bgFill = 'solid', borderWidth = 0, borderStyle = 'solid',
-  borderColor = '#8395a7',borderOpacity = 1, isParent = FALSE, labelColor = '#8395a7',
-  textOpacity = 1,fontSize = 16, textOutlineColor = '#222f3e', textOutlineOpacity = 1,
-  textOutlineWidth = 0,textbgColor = '#FFF', textbgOpacity = 0, textBorderColor = '#222f3e',
-  textBorderOpacity = 0,textBorderWidth = 0, parent = NULL, opacity = 1, pieSize = rep('0%',16), pieColor = rep('#000',16), tooltip = ''){
+buildNode <- function(
+                      id, width = 15, height = 15, shape = "ellipse", bgColor = "#48DBFB",
+                      bgOpacity = 1, bgFill = "solid", borderWidth = 0, borderStyle = "solid",
+                      borderColor = "#8395a7", borderOpacity = 1, isParent = FALSE, labelColor = "#8395a7",
+                      textOpacity = 1, fontSize = 16, textOutlineColor = "#222f3e", textOutlineOpacity = 1,
+                      textOutlineWidth = 0, textbgColor = "#FFF", textbgOpacity = 0, textBorderColor = "#222f3e",
+                      textBorderOpacity = 0, textBorderWidth = 0, parent = NULL, opacity = 1, pieSize = rep("0%", 16), pieColor = rep("#000", 16), tooltip = "") {
   # megaman SKY #48DBFB
   # storm petrel #8395A7
   # impreial primer #222F3E
 
-  l = list(group= 'nodes')
+  l <- list(group = "nodes")
 
-  options = list(
+  options <- list(
     width = width, height = height, label = id, id = id, shape = shape,
     bgColor = bgColor, bgOpacity = bgOpacity, bgFill = bgFill,
     borderWidth = borderWidth, borderStyle = borderStyle, borderColor = borderColor,
@@ -81,38 +87,40 @@ buildNode = function(
     pieColor9 = pieColor[9], pieColor10 = pieColor[10], pieColor11 = pieColor[11], pieColor12 = pieColor[12],
     pieColor13 = pieColor[13], pieColor14 = pieColor[14], pieColor15 = pieColor[15], pieColor16 = pieColor[16],
     tooltip = tooltip
-    )
+  )
 
-  if(isParent){ # parent node
-    options$bgColor = '#c8d6e5' # light blue ballerina
-    options$bgOpacity = '0.5'
+  if (isParent) { # parent node
+    options$bgColor <- "#c8d6e5" # light blue ballerina
+    options$bgOpacity <- "0.5"
   }
 
-  l$data = options
+  l$data <- options
 
   l
 }
 
 #' @export
-buildEdge = function(source, target, width = 3, curveStyle = 'haystack',
-                     lineColor = '#FECA57', lineStyle = 'solid', sourceArrowColor = '#feca57',
-                     targetArrowColor = '#feca57', sourceArrowShape = 'none', targetArrowShape = 'none',
-                     opacity = 1){
+buildEdge <- function(source, target, width = 3, curveStyle = "haystack",
+                      lineColor = "#FECA57", lineStyle = "solid", sourceArrowColor = "#feca57",
+                      targetArrowColor = "#feca57", sourceArrowShape = "none", targetArrowShape = "none",
+                      opacity = 1) {
   # casandora YELLOW #FECA57
-  l = list()
-  l$group = 'edges'
-  options = list(source = source, target = target, width = width,
-                 curveStyle=curveStyle, lineColor = lineColor, lineStyle = lineStyle,
-                 sourceArrowColor = sourceArrowColor, targetArrowColor = targetArrowColor,
-                 sourceArrowShape = sourceArrowShape, targetArrowShape = targetArrowShape,
-                 opacity = opacity)
-  l$data = options
+  l <- list()
+  l$group <- "edges"
+  options <- list(
+    source = source, target = target, width = width,
+    curveStyle = curveStyle, lineColor = lineColor, lineStyle = lineStyle,
+    sourceArrowColor = sourceArrowColor, targetArrowColor = targetArrowColor,
+    sourceArrowShape = sourceArrowShape, targetArrowShape = targetArrowShape,
+    opacity = opacity
+  )
+  l$data <- options
   l
 }
 
 #' @export
-viewElement = function(elements){
-  output = data.frame(
+viewElement <- function(elements) {
+  output <- data.frame(
     id = character(),
     type = factor(c("Node", "Edge")),
     style = list(),
@@ -121,18 +129,19 @@ viewElement = function(elements){
 }
 
 #' @export
-buildElems = function(elems, type){
-  func = paste0('build',type,'(')
+buildElems <- function(elems, type) {
+  func <- paste0("build", type, "(")
 
-  res = list()
-  params = colnames(elems)
-  for(i in 1:nrow(elems)){
-    command = c()
-    for(j in 1:length(params)){ command[j] = paste0(params[j], ' = ', "'",elems[i,j],"'") }
-    command = paste(command, collapse = ', ')
-    command = paste0(func ,command,')' )
-    res[[length(res)+1]] = eval(parse(text = command))
+  res <- list()
+  params <- colnames(elems)
+  for (i in 1:nrow(elems)) {
+    command <- c()
+    for (j in 1:length(params)) {
+      command[j] <- paste0(params[j], " = ", "'", elems[i, j], "'")
+    }
+    command <- paste(command, collapse = ", ")
+    command <- paste0(func, command, ")")
+    res[[length(res) + 1]] <- eval(parse(text = command))
   }
   return(res)
-
 }
